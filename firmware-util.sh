17762197cbbf392288ec795d9ff473f30d190aee
Bloxxness <113144430+Bloxxness@users.noreply.github.com>
2024-08-04 13:02:50 -0500

    Update firmware-util.sh

--- firmware-util.sh
+++ firmware-util.sh
@@ -15,7 +15,7 @@
 script_dir="$(dirname $(readlink -f $0))"
 
 #where the stuff is
-script_url="https://raw.githubusercontent.com/Bloxxness/the/main/"
+script_url="https://raw.githubusercontent.com/Bloxxness/the/master/"
 
 #ensure output of system tools in en-us for parsing
 export LC_ALL=C
